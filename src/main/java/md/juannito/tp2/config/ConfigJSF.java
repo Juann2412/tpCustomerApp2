e2d521e5b74daa186c3915481ad3ec0596195a2c
Juannito Ratovoniony <juannitoratovoniony9@gmail.com>
2022-04-03 16:45:33 +0200

    Etat Initial

---
+++ src/main/java/md/juannito/tp2/config/ConfigJSF.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package md.juannito.tp2.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author ASUS ROG
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
